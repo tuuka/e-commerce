b946e920a34e2863d83bbe2a4d84ad960c53db03
tuuka <scientist74@gmail.com>
2021-05-09 13:43:13 +0300

    All Tests GREEN state [ProductService some integration tests added]

---
+++ src/test/java/net/tuuka/ecommerce/service/ProductServiceIntegrationTest.java
@@ -0,0 +1,110 @@
+package net.tuuka.ecommerce.service;
+
+/*
+    ProductService should provide at least all CRUD operation on Product repository.
+    When saving and updating product must check Category correctness (it have to exist in DB already)
+    Mocking repositories here.
+*/
+
+import net.tuuka.ecommerce.dao.ProductCategoryRepository;
+import net.tuuka.ecommerce.dao.ProductRepository;
+import net.tuuka.ecommerce.entity.Product;
+import net.tuuka.ecommerce.exception.ProductCategoryNotFoundException;
+import net.tuuka.ecommerce.exception.ProductNotFoundException;
+import net.tuuka.ecommerce.util.FakeProductGenerator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.transaction.TestTransaction;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ProductServiceIntegrationTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductCategoryRepository productCategoryRepository;
+
+    @Autowired
+    ProductService productService;
+
+    List<Product> products;
+    Product product1, product2, product3;
+
+    @BeforeEach
+    void setUp() {
+        products = FakeProductGenerator.getNewFakeProductList(3, 3);
+        product1 = products.get(0);
+        product2 = products.get(1);
+        product3 = products.get(2);
+    }
+
+    @AfterEach
+    void tearDown() {
+        product1 = product2 = product3 = null;
+        productRepository.deleteAll();
+        productCategoryRepository.deleteAll();
+    }
+
+    @Test
+    void givenProductWithoutCat_whenSaveProduct_shouldSave() {
+
+        // given product with category
+        product1.setCategory(null);
+
+        // when save
+        Product savedProduct = productService.saveProduct(product1);
+        Product fetchedProduct = productService.getProductById(savedProduct.getId());
+
+        // then should get saved product back
+        assertEquals(savedProduct, fetchedProduct);
+        assertNull(fetchedProduct.getCategory());
+
+    }
+
+    @Test
+    void givenProductWithCatWithNullId_whenSaveProduct_shouldSave() {
+
+        assertThrows(ProductCategoryNotFoundException.class, () ->
+                productService.saveProduct(product1));
+
+    }
+
+
+    @Test
+    void givenNonExistingProductId_whenGetProductById_shouldThrowException() {
+
+        // given
+        long id = 999L;
+        // when
+        // then
+        assertThrows(ProductNotFoundException.class, () -> productService.getProductById(id));
+
+    }
+
+
+    private void commitAndBeginNewTransaction() {
+        if (TestTransaction.isActive()) {
+            TestTransaction.flagForCommit();
+            TestTransaction.end();
+        }
+        TestTransaction.start();
+    }
+
+    private void rollBack() {
+        if (TestTransaction.isActive()) {
+            TestTransaction.flagForRollback();
+            TestTransaction.end();
+        }
+    }
+
+}
