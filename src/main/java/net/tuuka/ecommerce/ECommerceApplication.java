b95155fcd608a4c909b39efa78bf8d5b03b7117d
tuuka <scientist74@gmail.com>
2021-05-09 16:39:30 +0300

    InitDB fail fixed

--- src/main/java/net/tuuka/ecommerce/ECommerceApplication.java
+++ src/main/java/net/tuuka/ecommerce/ECommerceApplication.java
@@ -3,15 +3,12 @@ package net.tuuka.ecommerce;
 import net.tuuka.ecommerce.dao.ProductCategoryRepository;
 import net.tuuka.ecommerce.dao.ProductRepository;
 import net.tuuka.ecommerce.entity.Product;
-import net.tuuka.ecommerce.entity.ProductCategory;
 import net.tuuka.ecommerce.util.FakeProductGenerator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ECommerceApplication {
@@ -23,10 +20,10 @@ public class ECommerceApplication {
     //    @Bean
     public CommandLineRunner initDB(ProductRepository productRepository, ProductCategoryRepository categoryRepository) {
         return (r) -> {
+            productRepository.deleteAll();
+            categoryRepository.deleteAll();
             List<Product> products = FakeProductGenerator.getNewFakeProductList(5, 3);
-            Set<ProductCategory> categories = products.stream().map(Product::getCategory).collect(Collectors.toSet());
-            categories.forEach(categoryRepository::save);
-            products.forEach(productRepository::save);
+            productRepository.saveAll(products);
         };
     }
 
