230687b60f8ccde03351fa85c2045d5953cd915b
tuuka <scientist74@gmail.com>
2021-05-24 18:47:43 +0300

    first step to integrate front-end.

---
+++ src/main/java/net/tuuka/ecommerce/controller/HomeController.java
@@ -0,0 +1,14 @@
+package net.tuuka.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
