af47d2fa4e790a1063c412938b2af654e44ed189
tuuka <scientist74@gmail.com>
2021-05-26 03:27:37 +0300

    setting angular

---
+++ src/main/frontend/src/app/model/Product.ts
@@ -0,0 +1,17 @@
+export class Product {
+
+  constructor(
+    public id: number,
+    public sku: string,
+    public name: string,
+    public description: string,
+    public unitPrice: number,
+    public imageUrl: string,
+    public active: boolean,
+    public unitsInStock: number,
+    public created: Date,
+    public lastUpdated: Date,
+    public category:object
+  ) {}
+
+}
---
+++ src/main/frontend/src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from "../model/Product";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.sass']
+})
+export class  ProductListComponent implements OnInit {
+
+  productList: Product[] = [
+    new Product(1,"sku1", "name1", "desc1", 1.0, "/img1.jpg", true, 10, new Date(), new Date(), {id:1, name:"cat1"}),
+    new Product(2, "sku2", "name2", "desc2", 1.0, "/img2.jpg", true, 10, new Date(), new Date(), {id:2, name:"cat2"}),
+    new Product(3, "sku3", "name3", "desc3", 1.0, "/img3.jpg", true, 10, new Date(), new Date(), {id:3, name:"cat3"})
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
