ad474e78d4d3189ac691316bbb1e5a93a8714c99
tuuka <scientist74@gmail.com>
2021-05-26 04:07:20 +0300

    set angular

---
+++ src/main/frontend/copyresources.js
@@ -0,0 +1,21 @@
+const shell = require('shelljs');
+
+module.exports = copyresources;
+copyresources.copy = copyresources;
+
+shell.echo('Copying *js and *.html file into resource folder')
+const source = process.argv[2];
+const dest = process.argv[3];
+copyresources(source, dest);
+
+function copyresources(source, target) {
+    // if (!shell.which('git')) {
+    //     shell.echo('Sorry, this script requires git');
+    //     shell.exit(1);
+    // }
+
+// shell.rm('-rf', 'out/Release');
+    shell.cp('-R', source + "/*.js", target + "/static/");
+    shell.cp('-R', source + "/*.css", target + "/static/");
+    shell.cp('-R', source + "/*.html", target + "/templates/");
+}
